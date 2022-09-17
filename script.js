9b1c101662ae5b887c27db6ae9edcde880d87d37
brodie02 <marshallbrodie0@gmail.com>
2022-09-17 17:39:23 +0930

    Add password variables

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to be?")
+  var lowercase = prompt("Would you like lowercase letters in your password?")
+  var uppercase = prompt("Would you like uppercase letters in your password?")
+  var numbers = prompt("Would you like numbers in your password?")
+  var special = prompt("Would you like special characters in your password?")
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
