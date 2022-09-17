a1761fcf1a321520ec9374758b8b3afde3d0d77c
brodie02 <marshallbrodie0@gmail.com>
2022-09-17 19:55:26 +0930

    Add new variables and conditional statements

--- script.js
+++ script.js
@@ -2,18 +2,67 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var passwordLength = prompt("How many characters would you like your password to be?")
-  while ((passwordLength <8)||
-  (passwordLength >128)) {
-    if ((passwordLength <8)||
-    (passwordLength >128)) {
-      passwordLength = prompt("Please choose between 8-128")
+  var chars = {
+    all: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#$%&*+/:;<=>?@\^_`{|}~",
+    notUpper: "abcdefghijklmnopqrstuvwxyz1234567890!#$%&*+/:;<=>?@\^_`{|}~",
+    notLower: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&*+/:;<=>?@\^_`{|}~",
+    notNumber: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&*+/:;<=>?@\^_`{|}~",
+    notSpecial: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890",
+    upperLower: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    upperNumber: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+    upperSpecial: "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+/:;<=>?@\^_`{|}~",
+    lowerNumber: "abcdefghijklmnopqrstuvwxyz1234567890",
+    lowerSpecial: "abcdefghijklmnopqrstuvwxyz!#$%&*+/:;<=>?@\^_`{|}~",
+    numberSpecial: "1234567890!#$%&*+/:;<=>?@\^_`{|}~",
+    Upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    Lower: "abcdefghijklmnopqrstuvwxyz",
+    Numbers: "1234567890",
+    Special: "!#$%&*+/:;<=>?@\^_`{|}~"
+  }
+
+  var pwdLength = prompt("How many characters would you like your password to be?")
+
+  while ((pwdLength <8)||
+  (pwdLength >128)) {
+    if ((pwdLength <8)||
+    (pwdLength >128)) {
+      pwdLength = prompt("Please choose between 8-128")
     }
   }
-  var lowercase = prompt("Would you like lowercase letters in your password?")
-  var uppercase = prompt("Would you like uppercase letters in your password?")
-  var numbers = prompt("Would you like numbers in your password?")
-  var special = prompt("Would you like special characters in your password?")
+
+  var pwdLowercase = confirm("Would you like lowercase letters in your password?")
+  var pwdUppercase = confirm("Would you like uppercase letters in your password?")
+  var pwdNumbers = confirm("Would you like numbers in your password?")
+  var pwdSpecial = confirm("Would you like special characters in your password?")
+
+  if (pwdLowercase && pwdUppercase && pwdNumbers && pwdSpecial) {
+      for (let i = 1; i <= pwdLength; i++) {
+        var char = Math.floor(Math.random() * chars.all.length + 1);
+        password += chars.all.charAt(char)
+    }
+  } else if (pwdLowercase && pwdUppercase && pwdNumbers) {
+      for (let i = 1; i <= pwdLength; i++) {
+        var char = Math.floor(Math.random() * chars.notSpecial.length + 1);
+        password += chars.notSpecial.charAt(char)
+      }
+  } else if (pwdLowercase && pwdUppercase && pwdSpecial) {
+    for (let i = 1; i <= pwdLength; i++) {
+      var char = Math.floor(Math.random() * chars.notNumber.length + 1);
+      password += chars.notNumber.charAt(char)
+    }
+  } else if (pwdNumbers && pwdUppercase && pwdSpecial) {
+    for (let i = 1; i <= pwdLength; i++) {
+      var char = Math.floor(Math.random() * chars.notLower.length + 1);
+      password += chars.notLower.charAt(char)
+    }
+  } else if (pwdNumbers && pwdLowercase && pwdSpecial) {
+    for (let i = 1; i <= pwdLength; i++) {
+      var char = Math.floor(Math.random() * chars.notUpper.length + 1);
+      password += chars.notUpper.charAt(char)
+    }
+  } 
+  return password
+
 }
 // Write password to the #password input
 function writePassword() {
